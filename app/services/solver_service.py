9c7e0d4964c84df8ef0c6a549886e396e529a842
cgnomc1 <cgnomc@gmail.com>
2025-04-11 14:43:41 +0100

    move graph.py to utils

--- app/services/solver_service.py
+++ app/services/solver_service.py
@@ -5,29 +5,8 @@ from typing import Dict, Any, List, Optional
 from collections import defaultdict, deque
 import heapq
 
-class Graph:
-    def __init__(self):
-        self.nodes = {}  # id -> Node object
-        self.edges = defaultdict(list)  # node_id -> [(node_id, weight)]
-        
-    @classmethod
-    def load_from_db(cls, db: Session):
-        graph = cls()
-        
-        # Load nodes
-        db_nodes = db.query(Node).all()
-        for node in db_nodes:
-            graph.nodes[node.id] = node
-        
-        # Load edges (vertices)
-        db_vertices = db.query(Vertex).all()
-        for vertex in db_vertices:
-            # Treat each vertex as an edge in both directions with weight=1
-            # This creates an undirected graph
-            graph.edges[vertex.parent_id].append((vertex.child_id, 1))
-            graph.edges[vertex.child_id].append((vertex.parent_id, 1))
-            
-        return graph
+from app.utils.graph import Graph
+
 
 class SolverService:
     @staticmethod
